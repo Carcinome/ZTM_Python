9310dd002ee8b38593b4d9257d4411f3cf3b6c89
carcinome <aicardic@home.arpa>
2025-09-05 15:51:43 +0200

    Course of numbers finished.

---
+++ numbers.py
@@ -0,0 +1,27 @@
+# Fundamental data types.
+
+# type for asking what the data type is.
+
+# integers:
+print(type(2 + 4))
+print(2 - 4)
+print(2 / 4)
+print(2 * 4)
+print(2 % 4)
+
+# For power of numbers, use **.
+print(2 ** 4) # = 16
+
+# Entire division, take the entire part of the division.
+print(2 // 4) # = 0
+
+# Modulo, remainder of the division. (remainder = reste)
+print(2 % 4) # = 2
+
+# float
+print(type(2 / 4))
+print(type(0.00002))
+print(type(10 + 1.1))
+print(type(9.9 + 1.1))
+
+# Need to more memory for stocking floats than integers.
