b6ce24544a617e396a4a53952bbf7cf631cea34e
carcinome <aicardic@home.arpa>
2025-09-05 17:01:45 +0200

    Add developers_fundamentals.md, pense-bete-markdown.md and continue numbers.py learning.

--- numbers.py
+++ numbers.py
@@ -9,7 +9,7 @@ print(2 / 4)
 print(2 * 4)
 print(2 % 4)
 
-# For power of numbers, use **.
+# For power of numbers, use **. (power of = puissance)
 print(2 ** 4) # = 16
 
 # Entire division, take the entire part of the division.
@@ -25,3 +25,14 @@ print(type(10 + 1.1))
 print(type(9.9 + 1.1))
 
 # Need to more memory for stocking floats than integers.
+
+# Math fonctions :
+# https://www.programiz.com/python-programming/modules/math
+
+# round, for rounding the number. (rounding = arrondir)
+print(round(3.1)) # = 3
+print(round(3.8)) # = 4
+
+# absolute, for absolute value of the argument (no negative number).
+print(abs(-20)) # = 20
+print(abs(-30.45)) # = 30.45
